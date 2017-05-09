5b6ff1950bb9c8d5d64471f02f25423b3bed723a
Ekta Khanna <ekhanna@pivotal.io>
2017-05-09 11:12:28 -0700

    CDBlize the cherry-pick
    Original Flow:
    cdb_flatten_sublinks
    	+--> pull_up_IN_clauses
    		+--> convert_sublink_to_join
    
    New Flow:
    cdb_flatten_sublinks
    	+--> pull_up_sublinks
    
    This commit contains relevant changes for the above flow.
    
    Previously, `try_join_unique` was part of `InClauseInfo`. It was getting
    set in `convert_IN_to_join()` and used in `cdb_make_rel_dedup_info()`.
    Now, since `InClauseInfo` is not present and we construct
    `FlattenedSublink` instead in `convert_ANY_sublink_to_join()`. And later
    in the flow, we construct `SpecialJoinInfo` from `FlattenedSublink` in
    `deconstruct_sublink_quals_to_rel()`. Hence, adding `try_join_unique` as
    part of both `FlattenedSublink` and `SpecialJoinInfo`.
    
    Ref [#142355175]
    
    Signed-off-by: Ekta Khanna <ekhanna@pivotal.io>

--- src/backend/nodes/outfuncs.c
+++ src/backend/nodes/outfuncs.c
@@ -1919,8 +1919,6 @@ _outUniquePath(StringInfo str, UniquePath *node)
 
 	_outPathInfo(str, (Path *) node);
 	WRITE_ENUM_FIELD(umethod, UniquePathMethod);
-	WRITE_NODE_FIELD(in_operators);
-	WRITE_NODE_FIELD(uniq_exprs);
 	WRITE_FLOAT_FIELD(rows, "%.0f");
     WRITE_BOOL_FIELD(must_repartition);                 /*CDB*/
     WRITE_BITMAPSET_FIELD(distinct_on_rowid_relids);    /*CDB*/
@@ -2233,6 +2231,7 @@ _outFlattenedSubLink(StringInfo str, FlattenedSubLink *node)
 	WRITE_BITMAPSET_FIELD(lefthand);
 	WRITE_BITMAPSET_FIELD(righthand);
 	WRITE_NODE_FIELD(quals);
+	WRITE_BOOL_FIELD(try_join_unique);	/*CDB*/
 }
 
 static void
@@ -2259,6 +2258,7 @@ _outInClauseInfo(StringInfo str, InClauseInfo *node)
     WRITE_BOOL_FIELD(try_join_unique);                  /*CDB*/
 	WRITE_NODE_FIELD(sub_targetlist);
 	WRITE_NODE_FIELD(in_operators);
+	WRITE_BOOL_FIELD(try_join_unique);	/*CDB*/
 }
 
 static void
