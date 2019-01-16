c5bcd94627350df0e69fb9551a4736d7d0f9b50a
David Kimura <dkimura@pivotal.io>
2019-01-16 11:26:23 -0800

    Cleanup artifacts of previous passing tests
    Co-authored-by: Ekta Khanna <ekhanna@pivotal.io>

--- src/bin/pg_rewind/sql/config_test.sh
+++ src/bin/pg_rewind/sql/config_test.sh
@@ -42,6 +42,17 @@ TESTROOT=$PWD/tmp_check_$TEST_SUITE
 TEST_MASTER=$TESTROOT/$TESTNAME/data_master
 TEST_STANDBY=$TESTROOT/$TESTNAME/data_standby
 
+# Remove data dirs from previous passing tests. Only retain the ones that
+# failed. We parse regression.out to determine which previous tests directories
+# can be deleted. Unfortunately this has the effect of leaving the last test
+# untouched. We could check the last test inside the Makefile if we wish to.
+if [ -f regression.out ]; then
+	PASSED_TEST=`tail -2 regression.out | grep -w "ok" | cut -d " " -f2`
+	if [ ! -z $PASSED_TEST ]; then
+		rm -rf $TESTROOT/$PASSED_TEST
+	fi
+fi
+
 # Create the root folder for test data
 mkdir -p $TESTROOT
 
